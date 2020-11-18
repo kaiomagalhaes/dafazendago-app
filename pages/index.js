3649c12c9a2575effad1c5252660bc105ce1dcc2
kaiomagalhaes <me@kaiomagalhaes.com>
2020-11-17 22:03:52 -0300

    add sort

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,24 @@ const getPrice = (price) => price / 100;
 export default function Home() {
   const categories = groupBy(json, 'category')
 
-  const data = Object.keys(categories).map((category) => (
+  const data = Object.keys(categories).sort().map((category) => (
     <div>
       <div key={category} className={styles['section-title']}>
         {category}
       </div>
       <div>
-        {categories[category].map((product) => (
+        {categories[category].sort((a, b) => {
+          const va = a.name[0];
+          const vb = b.name[0];
+          console.log(va, vb, va > vb, vb > va)
+          if (va > vb) {
+            return 1
+          } if (va < vb) {
+            return -1
+           }  else {
+            return 0
+          }
+        }).map((product) => (
           <div className={styles['item']} key={product.name}>
             <div className={styles['item-container']}>
             <span className={styles['name']}>{product.name}</span>
