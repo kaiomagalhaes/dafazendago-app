657891dbbc149535360ae7301ba67d14fc0ad35a
kaiomagalhaes <me@kaiomagalhaes.com>
2021-02-10 22:40:42 -0300

    fix foc

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
+import React, { useState, useEffect } from 'react';
 import styles from './index.module.css';
 import Categories from '../components/Categories';
 import Image from 'next/image'
 
 // <Image src="/hero.png" className={styles.hero} alt="hero image" width="300" height="64" />
 export default function Home() {
+  if (typeof window !== 'undefined') {
+    document.body.classList.add('no-fouc');
+  }
+
+  if (typeof window !== 'undefined') {
+    useEffect(() => {
+      const removeFouc = (foucElement) => {
+        foucElement.className = foucElement.className.replace('no-fouc', 'fouc');
+      };
+
+      removeFouc(document.documentElement);
+    }, []);
+  }
+
+
   return (
     <div className={styles['page-container']}>
-    <div className={styles['page']}>
+      <div className={styles['page']}>
 
-      <div className={styles['hero-section']}>
-        <img src={'https://i.ibb.co/cvVYNp5/kaio-3.jpg'} className={styles.logo} />
-      </div>
+        <div className={styles['hero-section']}>
+          <img src={'https://i.ibb.co/cvVYNp5/kaio-3.jpg'} className={styles.logo} />
+        </div>
 
-      <div className={styles["container"]}>
-        <Categories />
+        <div className={styles["container"]}>
+          <Categories />
+        </div>
       </div>
-    </div>
     </div>
   )
 }
